fa8f5998f0e5f1ed036fc98495d25632228210c5
koushik21 <sgogineni122@gmail.com>
2024-11-12 10:25:56 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+import.java.util.Scanner;
+class koushik{
+  public static void main(String[] args){
+    Scanner bot = new Scanner(System.in);
+    int n = bot.nextInt();
+    for(i=1;i<=;i++)
+      for(
